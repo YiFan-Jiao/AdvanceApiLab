405b9f32c270e23d9a25a152948ec03ff69e911d
YiFan <jiao1995cn@gmail.com>
2023-08-11 23:53:28 -0500

    lab3 done

---
+++ ControllersAndViews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControllersAndViews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
