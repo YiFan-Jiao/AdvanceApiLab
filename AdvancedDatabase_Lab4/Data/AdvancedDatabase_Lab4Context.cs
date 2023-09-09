59a2eaa6c4f0660195ea9c36a6488b596b71d044
YiFan <jiao1995cn@gmail.com>
2023-09-08 23:57:12 -0500

    lab4

---
+++ AdvancedDatabase_Lab4/Data/AdvancedDatabase_Lab4Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AdvancedDatabase_Lab4.Models;
+
+namespace AdvancedDatabase_Lab4.Data
+{
+    public class AdvancedDatabase_Lab4Context : DbContext
+    {
+        public AdvancedDatabase_Lab4Context (DbContextOptions<AdvancedDatabase_Lab4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AdvancedDatabase_Lab4.Models.Client> Client { get; set; } = default!;
+
+        public DbSet<AdvancedDatabase_Lab4.Models.Room> Room { get; set; } = default!;
+    }
+}
