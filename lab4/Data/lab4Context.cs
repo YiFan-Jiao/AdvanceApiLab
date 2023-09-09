9198b2277e22a8413f594f10c9a143d2ffbad3d7
YiFan <jiao1995cn@gmail.com>
2023-09-09 00:27:00 -0500

    lab4 done.

---
+++ lab4/Data/lab4Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using lab4.Models;
+
+namespace lab4.Data
+{
+    public class lab4Context : DbContext
+    {
+        public lab4Context (DbContextOptions<lab4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lab4.Models.Client> Client { get; set; } = default!;
+
+        public DbSet<lab4.Models.Room>? Room { get; set; } = default!;
+    }
+}
